4ac98e141eae1358061338459485b70e343f3e07
repparw <ubritos@gmail.com>
2024-09-30 13:06:09 -0300

    change branch back

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Neovim derivation";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     gen-luarc.url = "github:mrcjkb/nix-gen-luarc-json";
 
