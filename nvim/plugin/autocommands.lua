ae77ebd6d263368d5a265ec3acf2311921224f75
repparw <ubritos@gmail.com>
2024-10-03 20:20:53 -0300

    fix(lsp): format buffer keymap to spc fl

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -94,7 +94,7 @@ local lsp_attach = vim.api.nvim_create_autocmd('LspAttach', {
     keymap.set('n', '<M-l>', vim.lsp.codelens.run, desc('[lsp] run code lens'))
     keymap.set('n', '<space>cr', vim.lsp.codelens.refresh, desc('lsp [c]ode lenses [r]efresh'))
     keymap.set('n', 'gr', vim.lsp.buf.references, desc('lsp [g]et [r]eferences'))
-    keymap.set('n', '<space>f', function()
+    keymap.set('n', '<space>fl', function()
       vim.lsp.buf.format { async = true }
     end, desc('[lsp] [f]ormat buffer'))
     if client and client.server_capabilities.inlayHintProvider then
