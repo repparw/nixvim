9e4bb1a3bbc26c3692d4bbbad03f9d2b05ec2d54
repparw <ubritos@gmail.com>
2024-10-31 10:01:15 -0300

    copilot to claude sonnet

--- nvim/plugin/avante.lua
+++ nvim/plugin/avante.lua
@@ -1,6 +1,9 @@
 require('avante').setup {
-  -- recommended settings
   provider = 'copilot',
+  copilot = {
+    model = 'claude-3.5-sonnet',
+    -- max_tokens = 4096,
+  },
   auto_suggestions_provider = 'claude',
   hints = { enabled = false },
   behaviour = { auto_suggestions = false },
