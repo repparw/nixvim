e5e8c2407c146684ba0c4e8264c14560b801322d
repparw <ubritos@gmail.com>
2024-11-26 20:20:41 -0300

    change model to claude

--- nvim/plugin/avante.lua
+++ nvim/plugin/avante.lua
@@ -1,9 +1,8 @@
 require('avante').setup {
   provider = 'copilot',
-  --copilot = {
-  --model = 'claude-3.5-sonnet',
-  -- max_tokens = 4096,
-  --},
+  copilot = {
+    model = 'claude-3.5-sonnet',
+  },
   auto_suggestions_provider = 'claude',
   hints = { enabled = false },
   behaviour = { auto_suggestions = false },
